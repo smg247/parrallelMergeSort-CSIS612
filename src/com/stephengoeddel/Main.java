3ffe702df9bf1fdcdd5065c2c1dca754c31f0b22
Stephen Goeddel <sgoeddel247@gmail.com>
2019-02-06 09:21:36 -0500

    minor update to allow user to input threads and number of elements

--- src/com/stephengoeddel/Main.java
+++ src/com/stephengoeddel/Main.java
@@ -1,20 +1,20 @@
 package com.stephengoeddel;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-    private static int NUMBER_OF_THREADS = 28;
-    private static int NUMBER_OF_ELEMENTS = 1000000;
 
     public static void main(String[] args) throws InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many random elements would you like to sort?");
+        int numberOfElements = scanner.nextInt();
+        System.out.println("How many threads for the parallel version?");
+        int numberOfThreads = scanner.nextInt();
 
-        List<Integer> randomIntList = randomIntegerList(NUMBER_OF_ELEMENTS);
+        List<Integer> randomIntList = randomIntegerList(numberOfElements);
         List<Integer> nonParallelResult = benchmarkNonParallelVersion(new ArrayList<>(randomIntList));
-        List<Integer> parallelResult = benchmarkParallelVersion(new ArrayList<>(randomIntList));
+        List<Integer> parallelResult = benchmarkParallelVersion(new ArrayList<>(randomIntList), numberOfThreads);
 
         assert nonParallelResult.equals(parallelResult) : "Parallel and Sequential results did not match"; // Safety check, did both produce the same result?
         List<Integer> javaSortedResults = new ArrayList<>(nonParallelResult);
@@ -23,27 +23,27 @@ public class Main {
     }
 
     private static List<Integer> benchmarkNonParallelVersion(List<Integer> randomIntList) throws InterruptedException {
-        long startTime = System.currentTimeMillis();
+        long startTime = System.nanoTime();
         List<Integer> result = new ArrayList<>();
         Sorter<Integer> sorter = new Sorter<>(randomIntList, result);
         Thread sorterThread = new Thread(sorter);
         sorterThread.run();
         sorterThread.join();
-        long endTime = System.currentTimeMillis();
+        long endTime = System.nanoTime();
         System.out.println("Non-Parallel Version: ");
-        System.out.println("Sorted: " + result.size() + " elements in: " + (endTime - startTime) + " millis");
+        System.out.println("Sorted: " + result.size() + " elements in: " + (endTime - startTime) + " nano seconds");
 
         return result;
     }
 
-    private static List<Integer> benchmarkParallelVersion(List<Integer> randomIntList) throws InterruptedException {
-        long startTime = System.currentTimeMillis();
-        ParallelMergeSorter<Integer> parallelMergeSorter = new ParallelMergeSorter<>(randomIntList, NUMBER_OF_THREADS);
+    private static List<Integer> benchmarkParallelVersion(List<Integer> randomIntList, int numberOfThreads) throws InterruptedException {
+        long startTime = System.nanoTime();
+        ParallelMergeSorter<Integer> parallelMergeSorter = new ParallelMergeSorter<>(randomIntList, numberOfThreads);
         List<Integer> result = parallelMergeSorter.sort();
-        long endTime = System.currentTimeMillis();
+        long endTime = System.nanoTime();
 
         System.out.println("Parallel Version: ");
-        System.out.println("Sorted: " + result.size() + " elements in: " + (endTime - startTime) + " millis");
+        System.out.println("Sorted: " + result.size() + " elements in: " + (endTime - startTime) + " nano seconds");
 
         return result;
 
